0cb5e012b809cbbd2173ca7cc6f00c0f0ffc8cb4
Harshit Rajpal <harshitrajpal@Harshits-MacBook-Air.local>
2026-07-02 14:09:31 +1000

    updated logo and favicon

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'basanhomes@gmail.com'
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD', 'folkirdbbkwzpsqw')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD', 'folkirfdbkwzpsqw')
 app.config['MAIL_DEFAULT_SENDER'] = 'basanhomes@gmail.com'
 
 mail = Mail(app)
